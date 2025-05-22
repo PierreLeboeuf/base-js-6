5465e12a7208d047a1123d5b24be231b702e0e97
PierreLeboeuf <suckstone@gmail.com>
2025-05-22 12:33:15 +0200

    exo 7

---
+++ script.js
@@ -0,0 +1,97 @@
+// // ------------------------------------------
+// EXERCICE 1 : Créer et manipuler un tableau
+// // ------------------------------------------
+
+let couleurs = ["rouge", "vert", "bleu"];
+//Ajoute "jaune" au tableau.
+couleurs.push("jaune");
+console.log(couleurs);
+//Supprime la première couleur du tableau.
+couleurs.shift();
+console.log(couleurs);
+//Affiche toutes les couleurs avec une boucle.
+
+
+// // ------------------------------------------
+// EXERCICE 2 : Trouver un élément dans un tableau
+// // ------------------------------------------
+
+const nombres = [2, 7, 10, 23, 42];
+//vérifier si le nombre 10 est dans le tableau
+for (let chiffre of nombres) {
+    if (chiffre === 10) {
+        console.log(`10 est présent dans le tableau`);
+
+    } else {
+        console.log(`10 n'est pas présent`);
+    }
+}
+
+
+// // ------------------------------------------
+// EXERCICE 3 : Filtrer un tableau
+// // ------------------------------------------
+
+const ages = [12, 17, 22, 10, 34, 16];
+
+for (let age of ages) {
+    if (age >= 18) {
+        console.log(age);
+    }
+}
+
+// // ------------------------------------------
+// EXERCICE 4 : Inverser un tableau
+// // ------------------------------------------
+
+const jours = ["lundi", "mardi", "mercredi"];
+
+jours.splice(0, 1, "mercredi");
+jours.splice(2, 1, "lundi");
+console.log(jours);
+
+// // ------------------------------------------
+// EXERCICE 5 : Calculer la somme des éléments
+// // ------------------------------------------
+
+const notes = [15, 12, 17, 10, 8];
+let result = 0;
+
+for (let note of notes) {
+    console.log(note);
+    result = note + result
+}
+
+console.log(result);
+
+// // ------------------------------------------
+// EXERCICE 6 : Créer et manipuler un objet
+// // ------------------------------------------
+const livre = {
+    titre: "Le Petit Prince",
+    auteur: "Antoine de Saint-Exupéry",
+    pages: 96
+};
+//Affiche le titre et l’auteur dans la console.
+console.log(livre.titre +" de "+livre.auteur);
+//Modifie le nombre de pages à 100.
+livre.pages=100;
+console.log(livre.pages);
+//Ajoute une propriété langue avec la valeur "français".
+livre.langue = "français"
+console.log(livre);
+
+
+// // ------------------------------------------
+// EXERCICE 7 : Parcourir un objet
+// // ------------------------------------------
+
+const personne = {
+    nom: "Alice",
+    age: 25,
+    ville: "Paris"
+};
+
+for (cle in personne) {
+    console.log(cle + ": " + personne[cle]);
+}
